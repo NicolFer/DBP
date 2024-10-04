d7adc8c4976b29af9ae029ad35e690b8c574990b
NicolFer <164351032+NicolFer@users.noreply.github.com>
2024-10-04 02:42:27 -0500

    Add files via upload

---
+++ todolis1/app.py
@@ -0,0 +1,15 @@
+import requests
+
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/convert", methods=["POST"])
+def convert():
+    
